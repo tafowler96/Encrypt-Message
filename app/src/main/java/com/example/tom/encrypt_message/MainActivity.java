026e24116e759f4537fdf3c5b2b56b0c53a2ad7f
Tom Fowler <tafowler96@gmail.com>
2017-03-15 16:22:16 -0400

    various updates

--- app/src/main/java/com/example/tom/encrypt_message/MainActivity.java
+++ app/src/main/java/com/example/tom/encrypt_message/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +15,7 @@ import java.security.Key;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
 import java.security.SecureRandom;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.RSAKeyGenParameterSpec;
@@ -33,16 +36,44 @@ public class MainActivity extends AppCompatActivity {
         writeKeyPairToPreferences(pair);
 
         Button encryptButton = (Button) findViewById(R.id.encrypt);
-        Button decryptButton = (Button) findViewById(R.id.decrypt);
+        final Button decryptButton = (Button) findViewById(R.id.decrypt);
 
         encryptButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 byte[] message = ((EditText) findViewById(R.id.message)).getText().toString().getBytes();
                 byte[] encrypted = encrypt(pair.getPublic(), message);
-                ((TextView)findViewById(R.id.encryptedMessage)).setText(encrypted.toString());
+                byte[] encoded = Base64.encode(encrypted, Base64.DEFAULT);
+                ((TextView)findViewById(R.id.encryptedMessage)).setText(bytesToHex(encoded));
             }
         });
+        decryptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = ((TextView) findViewById(R.id.encryptedMessage)).getText().toString();
+                byte[] decoded = Base64.decode(message, Base64.DEFAULT);
+                byte[] decrypted = decrypt(pair.getPrivate(), decoded);
+                ((TextView)findViewById(R.id.encryptedMessage)).setText(new String(decrypted));
+            }
+        });
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuffer result = new StringBuffer();
+        for (byte b : bytes) result.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+        return result.toString();
+    }
+
+    private byte[] decrypt(PrivateKey privKey, byte[] message) {
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/NONE/OAEPWithSHA256AndMGF1Padding", "BC");
+            cipher.init(Cipher.DECRYPT_MODE, privKey);
+//            cipher.update(plaintext);
+            return cipher.doFinal(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     public static KeyPair generate() {
@@ -50,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
             SecureRandom sr = new SecureRandom();
             RSAKeyGenParameterSpec spec = new RSAKeyGenParameterSpec(KEY_SIZE, RSAKeyGenParameterSpec.F4);
             KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA", "BC");
-            gen.initialize(spec, sr);
+            gen.initialize(KEY_SIZE);
             KeyPair pair = gen.generateKeyPair();
 //            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(pair.getPrivate().getEncoded());
             return pair;
@@ -66,10 +97,11 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("privateKey", pair.getPrivate().toString());
     }
 
-    public byte[] encrypt(Key pubKey, byte[] plaintext) {
+    private byte[] encrypt(Key pubKey, byte[] plaintext) {
         try {
             Cipher cipher = Cipher.getInstance("RSA/NONE/OAEPWithSHA256AndMGF1Padding", "BC");
             cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+            Log.v("Format", pubKey.getFormat());
 //            cipher.update(plaintext);
             return cipher.doFinal(plaintext);
         } catch (Exception e) {
